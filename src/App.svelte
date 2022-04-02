2e2cd1d8881c6f78bda7008d69db456c55009184
Renam Freitas <renam.freitas@decathlon.com>
2022-04-02 22:45:55 +0100

    feat: meetupItem component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,46 @@
 <script>
   import Header from './UI/Header.svelte'
+  import MeetupItem from './Meetups/MeetupItem.svelte'
+
+  const meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      description: 'In this meetup we will have some experts that teach you how to code!',
+      imageUrl: 'https://picsum.photos/seed/m1/600/300',
+      address: '27th  Nerd Road, 32523 New York',
+      contactEmail: 'code@test.com'
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Let\'go for some swimming',
+      description: 'We will simply swim some rounds!',
+      imageUrl: 'https://picsum.photos/seed/m2/600/300',
+      address: '27th  Nerd Road, 32523 New York',
+      contactEmail: 'swim@test.com'
+    }
+  ]
 </script>
 
+<style>
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>
+
 <Header />
+
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetupItem 
+      title="{meetup.title}"
+      subtitle="{meetup.subtitle}"
+      description="{meetup.description}"
+      imageUrl="{meetup.imageUrl}"
+      address="{meetup.address}"
+      contactEmail="{meetup.contactEmail}"
+    />
+  {/each}
+</section>
---
+++ src/Meetups/MeetupItem.svelte
@@ -0,0 +1,81 @@
+<script>
+  export let title
+  export let subtitle
+  export let description
+  export let imageUrl
+  export let address
+  export let contactEmail
+</script>
+
+<style>
+  article {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    background: white;
+    margin: 1rem;
+  }
+
+  header,
+  .content,
+  footer {
+    padding: 1rem;
+  }
+
+  .image {
+    width: 100%;
+    height: 14rem;
+  }
+
+  .image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  h1 {
+    font-size: 1.25rem;
+    margin: 0.5rem 0;
+    font-family: "Roboto Slab", sans-serif;
+  }
+
+  h1.is-favorite {
+    background: #01a129;
+    color: white;
+    padding: 0 0.5rem;
+    border-radius: 5px;
+  }
+
+  h2 {
+    font-size: 1rem;
+    color: #808080;
+    margin: 0.5rem 0;
+  }
+
+  p {
+    font-size: 1.25rem;
+    margin: 0;
+  }
+
+  div {
+    text-align: right;
+  }
+</style>
+
+<article>
+  <header>
+    <h1>{title}</h1>
+    <h2>{subtitle}</h2>
+    <p>{address}</p>
+  </header>
+  <div class="image">
+    <img src="{imageUrl}" alt="{title}">
+  </div>
+  <div class="content">
+    <p>{description}</p>
+  </div>
+  <footer>
+    <a href="mailto:{contactEmail}">Send e-mail</a>
+    <button>Show Details</button>
+    <button>Favorite</button>
+  </footer>
+</article>
