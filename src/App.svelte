4e8a7a3bb942edcf9f17d34ed582f4c18ff4f941
Renam Freitas <renam.freitas@decathlon.com>
2022-04-03 00:29:52 +0100

    style: input component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from './UI/Header.svelte'
   import MeetupGrid from './Meetups/MeetupGrid.svelte'
+  import TextInput from './UI/TextInput.svelte'
 
   let title = ''
   let subtitle = ''
@@ -50,36 +51,60 @@
     margin-top: 5rem;
   }
 
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
+
 </style>
 
 <Header />
 
 <main>
   <form on:submit|preventDefault="{addMeetup}">
-    <div class="form-control">
-      <label for="title">Title</label>
-      <input type="text" id="title" bind:value="{title}">
-    </div>
-    <div class="form-control">
-      <label for="subtitle">Subtitle</label>
-      <input type="text" id="subtitle" bind:value="{subtitle}">
-    </div>
-    <div class="form-control">
-      <label for="address">Address</label>
-      <input type="text" id="address" bind:value="{address}">
-    </div>
-    <div class="form-control">
-      <label for="imageUrl">Image Url</label>
-      <input type="text" id="imageUrl" bind:value="{imageUrl}">
-    </div>
-    <div class="form-control">
-      <label for="email">Email</label>
-      <input type="email" id="email" bind:value="{email}">
-    </div>
-    <div class="form-control">
-      <label for="description">Description</label>
-      <textarea rows="3" id="description" bind:value="{description}" />
-    </div>
+    <TextInput 
+      id="title"
+      label="Title"
+      type="text"
+      value={title} 
+      on:input={event => (title = event.target.value)}
+    />
+    <TextInput 
+      id="subtitle"
+      label="Subitle"
+      type="text"
+      value={subtitle} 
+      on:input={event => (subtitle = event.target.value)}
+    />
+    <TextInput 
+      id="address"
+      label="Address"
+      type="text"
+      value={address} 
+      on:input={event => (address = event.target.value)}
+    />
+    <TextInput 
+      id="imageUrl"
+      label="Image"
+      type="text"
+      value={imageUrl} 
+      on:input={event => (imageUrl = event.target.value)}
+    />
+    <TextInput 
+      id="email"
+      label="E-mail"
+      type="email"
+      value={email} 
+      on:input={event => (email = event.target.value)}
+    />
+    <TextInput 
+      id="description"
+      label="Description"
+      controlType="textarea"
+      value={description} 
+      on:input={event => (description = event.target.value)}
+    />
     <button type="submit">Save</button>
   </form>
   
---
+++ src/UI/TextInput.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let controlType
+  export let id
+  export let label
+  export let rows
+  export let value
+  export let type
+</script>
+
+<style>
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: none;
+    border-bottom: 2px solid #ccc;
+    border-radius: 3px 3px 0 0;
+    background: white;
+    padding: 0.15rem 0.25rem;
+    transition: border-color 0.1s ease-out;
+  }
+
+  input:focus,
+  textarea:focus {
+    border-color: #e40763;
+    outline: none;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+    width: 100%;
+  }
+
+  .form-control {
+    padding: 0.5rem 0;
+    width: 100%;
+    margin: 0.25rem 0;
+  }
+</style>
+
+<div class="form-control">
+  <label for="{id}">{label}</label>
+  {#if controlType === 'textarea'}
+    <textarea rows="{rows}" id="{id}" value="{value}" on:input />
+  {:else}
+    <input type="{type}" id="{id}"  value="{value}" on:input>
+  {/if}
+</div>
